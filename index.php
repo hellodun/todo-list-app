8d61496bb5aaac56d9b1ea788cb605f4c1d41e35
Zain Ahmed <zainahmed1713@gmail.com>
2023-06-05 09:03:06 +0500

    Added links for signup and login page

--- index.php
+++ index.php
@@ -1,41 +1,39 @@
 <!DOCTYPE html>
 <html lang="en">
-  <head>
-    <meta charset="UTF-8" />
-    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <link rel="preconnect" href="https://fonts.googleapis.com" />
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
-    <link
-      href="https://fonts.googleapis.com/css2?family=Poppins:wght@500&display=swap"
-      rel="stylesheet"
-    />
-    <link rel="stylesheet" href="style.css" />
-    <title>DoToday - Todo List App</title>
-  </head>
-  <body>
-    <nav class="navbar">
-      <div class="nav-heading">
-        <h1>DoToday</h1>
-      </div>
-      <div class="nav-links">
-        <ul>
-          <li><a href="index.html">Home</a></li>
-          <li><a href="#">Login</a></li>
-          <li>
-            <a href="index.html"><button class="signup-btn">Signup</button></a>
-          </li>
-        </ul>
-      </div>
-    </nav>
-    <div class="container">
-      <header class="header">
-        <h3>Why waste time for something that you can DoToday!</h3>
-        <p>The <strong>#1</strong> Free App to Organize Your Whole Life</p>
-        <a href="#"
-          ><button class="get-started">Get Started For Free</button></a
-        >
-      </header>
+
+<head>
+  <meta charset="UTF-8" />
+  <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <link rel="preconnect" href="https://fonts.googleapis.com" />
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+  <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@500&display=swap" rel="stylesheet" />
+  <link rel="stylesheet" href="style.css" />
+  <title>DoToday - Todo List App</title>
+</head>
+
+<body>
+  <nav class="navbar">
+    <div class="nav-heading">
+      <h1>DoToday</h1>
     </div>
-  </body>
+    <div class="nav-links">
+      <ul>
+        <li><a href="index.html">Home</a></li>
+        <li><a href="login.php">Login</a></li>
+        <li>
+          <a href="signup.php"><button class="signup-btn">Signup</button></a>
+        </li>
+      </ul>
+    </div>
+  </nav>
+  <div class="container">
+    <header class="header">
+      <h3>Why waste time for something that you can DoToday!</h3>
+      <p>The <strong>#1</strong> Free App to Organize Your Whole Life</p>
+      <a href="signup.php"><button class="get-started">Get Started For Free</button></a>
+    </header>
+  </div>
+</body>
+
 </html>
