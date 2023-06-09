b4e6c718597095aa3e26b985871321419d55896a
Zain Ahmed <zainahmed1713@gmail.com>
2023-06-10 02:24:51 +0500

    Added DB connectivity file

---
+++ db_conn.php
@@ -0,0 +1,11 @@
+<?php
+$host = "localhost";
+$username = "root";
+$password = "";
+$db_name = "dotoday_db";
+
+$conn = mysqli_connect($host, $username, $password, $db_name);
+
+if (!$conn) {
+    die("Error: " . mysqli_connect_error());
+}
