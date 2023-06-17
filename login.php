8955f6b9498de63bc0bef9260aef73ec58f94dab
Zain Ahmed <zainahmed1713@gmail.com>
2023-06-17 19:00:47 +0500

    Fixed bug that was stopping users to login to the app

--- login.php
+++ login.php
@@ -1,17 +1,3 @@
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="preconnect" href="https://fonts.googleapis.com" />
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
-    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@500&display=swap" rel="stylesheet" />
-    <link rel="stylesheet" href="style.css">
-    <title>Login</title>
-</head>
-
 <?php
 $invalid_login = false;
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
@@ -40,6 +26,19 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 }
 
 ?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="preconnect" href="https://fonts.googleapis.com" />
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@500&display=swap" rel="stylesheet" />
+    <link rel="stylesheet" href="style.css">
+    <title>Login</title>
+</head>
 
 <body>
     <nav class="navbar">
